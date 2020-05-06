c35c12d5ca74b02b44f82919b1f9e5ea2b7996e2
Sanjay Patil <sp2004@hotmail.com>
2020-05-06 13:30:10 -0700

    Sanjay-Added Login logic using passport

--- server.js
+++ server.js
@@ -5,7 +5,37 @@ const bodyParser = require('body-parser')
 
 const messages = require('./routes/api/messages')
 
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const routes = require('./routes/api/auth-routes');
+require("dotenv").config();
+// const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./passport-setup')
+const keys = require('./config/keys');
+const cors = require('cors')
 const app = express();
+
+///setup CORS
+app.use(
+    cors({
+      origin: "http://localhost:3001", // allow to server to accept request from different origin
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true // allow session cookie from browser to pass through
+    })
+  );
+
+
+
+//setup cookie
+
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.googleKey.cookiekey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 //bodyparser middleware
 app.use(bodyParser.json());
@@ -28,12 +58,15 @@ if(process.env.NODE_ENV ==="production"){
 const mongodb = require('./config/keys').mLab.MONGODB_URI;
 
 // Connect the Mongo
-mongoose.connect(process.env.MONGODB_URI ||  mongodb, {useNewUrlParser: true})
-.then(()=>console.log("mongodb connected..."))
-.catch(()=> console.log(err));
+// mongoose.connect(process.env.MONGODB_URI ||  mongodb, {useNewUrlParser: true})
+// .then(()=>console.log("mongodb connected..."))
+// .catch(()=> console.log(err));
+
+mongoose.connect("mongodb://localhost/watsonDB", { useNewUrlParser: true });
 
 //use Routes
 app.use('/api/messages', messages)
+app.use(routes);
 
 
 
