393a2bab976cfed9d2922740a9c33b7d648390ab
Sanjay Patil <sp2004@hotmail.com>
2020-05-09 08:00:44 -0700

    added fancy button for social media login

--- client/src/pages/google.js
+++ client/src/pages/google.js
@@ -8,14 +8,32 @@ class Google extends Component {
 
     googleLogin = (event) => {
         // console.log(event.target.value)
-        console.log("in google button press") 
+        console.log("in google button press")
         API.googleLogin()
     }
 
 
     render() {
         return (
-            <button type="button" onClick={this.googleLogin}>Google+</button>
+            // <div class="google-btn">
+            //     <div class="google-icon-wrapper">
+            //         <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+            //     </div>
+            //     <p class="btn-text" onClick={this.googleLogin}><b>Sign in with google</b></p>
+            // </div>
+            // // <button type="button" onClick={this.googleLogin}>Google+</button>
+
+            <div class="col">
+                <a href="#" class="fb btn">
+                    <i class="fa fa-facebook fa-fw"></i> Login with Facebook
+         </a>
+                <a href="#" class="twitter btn">
+                    <i class="fa fa-twitter fa-fw"></i> Login with Twitter
+        </a>
+                <a href="#" class="google btn" onClick={this.googleLogin}><i class="fa fa-google fa-fw">
+                </i> Login with Google+
+        </a>
+            </div>
         )
 
     }
