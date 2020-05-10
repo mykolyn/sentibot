9096744c0a46199ec1e3535711f92e0e9d026b42
xiongkb <mkan-bx@hotmail.com>
2020-05-10 14:37:14 -0700

    testing connection

--- routes/api/watson.js
+++ routes/api/watson.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 require("dotenv").config();
-
-//const keys = require("../config/keys");
 console.log('test');
 
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
@@ -14,9 +12,6 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   }),
   url: process.env.REACT_APP_watsonURL,
 });
-console.log("watson.js")
-console.log(process.env.REACT_APP_watsonURL)
-console.log(process.env.REACT_APP_watsonKey)
 
 router.post('/analyzer', (req,res) => {
   const analyzeParams = {
